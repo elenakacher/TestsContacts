f8a3d8d4b981d15387934b40c1b45c093eab5054
elenakacher <kacheren961@gmail.com>
2022-08-08 14:48:52 +0200

    HomePageTests

--- src/test/java/com/telran/contacts/tests/HomePageTests.java
+++ src/test/java/com/telran/contacts/tests/HomePageTests.java
@@ -1,5 +1,7 @@
 package com.telran.contacts.tests;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomePageTests extends TestBase {
@@ -9,4 +11,25 @@ public class HomePageTests extends TestBase {
         System.out.println("Site Opened!");
         app.isComponentFormPresent();
     }
+
+    @Test
+    public void PhonebookTest(){
+        Assert.assertTrue(app.getHeader().isElementPresent(By.xpath("//h1[contains(text(),'PHONEBOOK')]")));
+    }
+
+    @Test
+    public void HomeTest(){
+        Assert.assertTrue(app.getHeader().isElementPresent(By.xpath("//a[contains(text(),'HOME')]")));
+    }
+
+    @Test
+    public void AboutTest(){
+        Assert.assertTrue(app.getHeader().isElementPresent(By.xpath("//a[contains(text(),'ABOUT')]")));
+    }
+
+    @Test
+    public void LoginTest(){
+        Assert.assertTrue(app.getHeader().isElementPresent(By.xpath("//a[contains(text(),'LOGIN')]")));
+    }
+
 }
